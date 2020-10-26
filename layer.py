053fa72498c279c1719e43295a342e6e4cd165de
William Simoni <56754601+WilliamSimoni@users.noreply.github.com>
2020-10-26 08:47:41 +0100

    Added checkings and removed parameters
    I added the checking block to be sure that the parameters sent to the constructor are right. Besides, I removed the parameters num_input and num_unit; the constructor calculates automatically both the num_unit and num_input.

--- layer.py
+++ layer.py
@@ -2,43 +2,54 @@
     Layer Module used to represent a layer of a NN
 """
 import numpy as np
-
+import activationFunction
+import weightInitializer as wi
 
 class Layer:
     """
         Layer class represent a layer in a NN
     """
 
-    def __init__(self, weights, learning_rates, num_unit, num_input, activation):
+    def __init__(self, weights, learning_rates, activation):
         """This function initialize an instance of the layer class
 
         Args:
-            weights (numpy.ndarray): matrix, of num_unit * num_input + 1 elements, that contain the weights of the units in the layer
-            learning_rates (numpy.ndarray): matrix, of unitNumber * inputNumber elements, that contain the learning rates associated with the units in the layer
-            num_unit (int): number of hidden units that compose the layer
-            num_unit (int): number of input for each unit of the layer
+            weights (numpy.ndarray): matrix, of num_unit * num_input + 1 elements, 
+            that contain the weights of the units in the layer (including the biases)
+
+            learning_rates (numpy.ndarray): matrix, of unitNumber * inputNumber elements, 
+            that contain the learning rates of the units in the layer (including the biases)
+
             activation (ActivationFunction): each unit of this layer use this function as activation function
         """
 
         # checking parameters -------------------
-        # TODO
+        if not isinstance(weights, np.ndarray):
+            raise ValueError('weights must be a np.ndarray object')
+        if not isinstance(learning_rates, np.ndarray):
+            raise ValueError('learning_rates must be a np.ndarray object')
+        if not isinstance(activation, activationFunction.Activation_function):
+            raise ValueError('activation must be an activation function') 
+
+        if weights.shape != learning_rates.shape:
+            raise ValueError('weights and learning_rates must have the same shape')
         # ---------------------------------------
 
         self.weights = weights
         self.learning_rates = learning_rates
-        self.num_unit = num_unit
-
-        # num_input + 1 because there are num_input inputs + the bias input
-        self.num_input = num_input + 1
-
         self.activation = activation
+
+        # num_unit = number of weights'/learning_rates' rows
+        # num_input = number of weights'/learning_rates' columns
+        self.num_unit, self.num_input = weights.shape
 
     def function_signal(self, input_values):
         """
             Calculate the propagated values of a layer using an activation function
 
             Parameters:
-                input_values: values used as input in a Layer (it is the output of predicing layer)
+                input_values: values used as input in a Layer (must have 1 at the beginning) 
+                    (it is the output of predicing layer)
 
             Return: output values of Layer units
         """
