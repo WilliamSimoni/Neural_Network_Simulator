2158bd30e41afcb3071507e8bbe2e1a9b5dbba62
William Simoni <56754601+WilliamSimoni@users.noreply.github.com>
2020-10-26 10:48:58 +0100

    weight initializer added
    Contains method to initialize the weights in a layer

---
+++ weightInitializer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def xavier_initializer(num_input, num_unit):
+    """returns weight matrix to use in a layer (included bias in the first column)
+
+        The xavier method apply the following rule:
+        - w[i][j] = random() * 1/sqrt(num_input)
+        - bias = 0
+
+    Args:
+        num_input (int): number of input of the layer (no bias included in the counting)
+        num_unit (int): number of unit in the layer
+
+    Returns:
+        numpy.ndarray: returns weight matrix to use in a layer (included bias in the first column)
+    """
+    bias_weights = np.zeros((num_unit,1))
+    input_weights = np.random.normal(size=[num_unit,num_input]) * np.sqrt(1/num_input)
+    return np.concatenate((bias_weights, input_weights), axis=1)
