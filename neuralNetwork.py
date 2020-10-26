8ec4410a4fec73102c86af14f47144d2757bb6ae
William Simoni <56754601+WilliamSimoni@users.noreply.github.com>
2020-10-26 10:45:20 +0100

    Added constructor and new method addLayer
    addLayer adds a new layer in the neural network. (Probably, it will be changed when we will introduce output and hidden layers specialization)

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -9,6 +9,65 @@ class NeuralNetwork:
         Neural Network class to represent a feedforward Neural Network
     """
 
+    def __init__(self, max_epochs):
+        """create an instance of neural network class
+
+        Parameters:
+            max_epochs (int): maximum number of epochs for the training
+        """
+
+        # checking parameters -------------------
+        # TODO
+        # ---------------------------------------
+
+        self.max_epochs = max_epochs
+
+        self.input_dimension = 0
+        self.output_dimension = 0
+
+        # note: this is not a np.ndarray object
+        self.layers = []
+
+    def addLayer(self, layer):
+        
+        """ add a layer in the neural network
+
+            Parameters:
+                layer (Layer): layer to be added. The layer must have a number 
+                of input equal to the unit of the previous layer
+
+            Raises:
+                ValueError: the layer is not a Layer object
+                ValueError: The number of input for this new layer is not equal 
+                  to the number of unit of the previous layer in the neural network
+                  
+            Example:  
+                this is a neural network with two layers
+
+                      o   o   o
+                    o   o   o   o
+
+                Then we execute neuralNetwork.addLayer(layer) where layer has 2 units with 3 inputs(o o):
+
+                        o   o
+                      o   o   o
+                    o   o   o   o
+
+        """
+
+        if not isinstance(layer, Layer):
+            raise ValueError('layer must be a Layer object')
+
+        if len(self.layers) == 0:
+            self.input_dimension = layer.get_num_input()
+        elif layer.get_num_input() != self.output_dimension:
+            raise ValueError(
+                "The number of input for this new layer must be equal to the number of unit of the previous layer")
+
+        self.output_dimension = layer.get_num_unit()
+
+        self.layers.append(layer)
+
     def predict(self, sample):
         """
             Predict method implement the predict operation to make prediction
@@ -35,12 +94,12 @@ class NeuralNetwork:
 
             Precondition:
                 The length of sample is equal to input dimension in NN
-              
+
             Return: the predicted output obtained after propagation of signal
         """
         if len(sample) != self.input_dimension:
             raise ValueError
-   
+
         input_layer = sample
 
         for layer in self.layers:
