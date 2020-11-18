3add06ec76a3e873abdd0955b0928e5365682179
William Simoni <wilsimoni@gmail.com>
2020-11-18 10:25:17 +0100

    added he_initializer, and other weight initializer
    The other weight initializer are:
    * all_zero_initializer: create a matrix with only 0
    * big_random_initializer: create a matrix with big weights

--- weightInitializer.py
+++ weightInitializer.py
@@ -4,6 +4,7 @@
 """
 import numpy as np
 
+
 def xavier_initializer(num_unit, num_input):
     """returns weight matrix to use in a layer (included bias in the first column)
 
@@ -19,5 +20,62 @@ def xavier_initializer(num_unit, num_input):
         numpy.ndarray: returns weight matrix to use in a layer (included bias in the first column)
     """
     bias_weights = np.zeros((num_unit, 1))
-    input_weights = np.random.normal(size=[num_unit, num_input]) * np.sqrt(1/num_input)
+    input_weights = np.random.randn(num_unit, num_input) * np.sqrt(1/num_input)
+    return np.concatenate((bias_weights, input_weights), axis=1)
+
+
+def he_initializer(num_unit, num_input):
+    """returns weight matrix to use in a layer (included bias in the first column)
+
+        The he method apply the following rule:
+        - w[i][j] = random() * sqrt(2/num_input)
+        - bias = 0
+
+    Args:
+        num_unit (int): number of unit in the layer
+        num_input (int): number of input of the layer (no bias included in the counting)
+
+    Returns:
+        numpy.ndarray: returns weight matrix to use in a layer (included bias in the first column)
+    """
+    bias_weights = np.zeros((num_unit, 1))
+    input_weights = np.random.randn(num_unit, num_input) * np.sqrt(2/num_input)
+    return np.concatenate((bias_weights, input_weights), axis=1)
+
+
+def all_zero_initializer(num_unit, num_input):
+    """
+    WARNING: ONLY FOR TESTING, BAD TO USE
+
+    returns weight matrix to use in a layer (included bias in the first column).
+    All elements are initialized to zero
+
+    Args:
+        num_unit (int): number of unit in the layer
+        num_input (int): number of input of the layer (no bias included in the counting)
+
+    Returns:
+        numpy.ndarray: returns weight matrix to use in a layer (included bias in the first column)
+    """
+    bias_weights = np.zeros((num_unit, 1))
+    input_weights = np.zeros((num_unit, num_input))
+    return np.concatenate((bias_weights, input_weights), axis=1)
+
+
+def big_random_initializer(num_unit, num_input):
+    """
+    WARNING: ONLY FOR TESTING, BAD TO USE
+
+    returns weight matrix to use in a layer (included bias in the first column).
+    All elements are initialized to random()*10
+
+    Args:
+        num_unit (int): number of unit in the layer
+        num_input (int): number of input of the layer (no bias included in the counting)
+
+    Returns:
+        numpy.ndarray: returns weight matrix to use in a layer (included bias in the first column)
+    """
+    bias_weights = np.zeros((num_unit, 1))
+    input_weights = np.random.randn(num_unit, num_input) * 10
     return np.concatenate((bias_weights, input_weights), axis=1)
