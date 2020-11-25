a946b59eaaf4fee0a907a169a8547eccab23b282
bigboss98 <=>
2020-11-25 20:07:11 +0100

    Implement valid Cross Validation

--- cross_validation.py
+++ cross_validation.py
@@ -1,22 +1,20 @@
 import numpy as np
 import copy
+import math
 from utility import read_monk_data
 from neural_network import NeuralNetwork
 from layer import OutputLayer, HiddenLayer
 import weight_initializer as wi
 import activation_function as af
 
+
 def cross_validation(model, dataset, num_subsets):
-    splitted_list_set = np.array_split(dataset, num_subsets)
-    splitted_training_set = [tuple(x)  for split_elem in splitted_list_set
-                                       for x in splitted_list_set]
-    weights = model.layers[0].get_weights()
+    num_elem = math.ceil(len(dataset)/num_subsets)
 
     for k in range(0, num_subsets):
         model_k = copy.deepcopy(model)
-        training_set = np.array([np.append(splitted_training_set[:k], splitted_training_set[k+1:])])
-        validation_set = splitted_training_set[k]
-        #print(training_set[0][0])
+        training_set = dataset[:k*num_elem] + dataset[(k+1)*num_elem:]
+        validation_set = dataset[k*num_elem:(k+1)*num_elem]
         report = model_k.fit(training_set, validation_set)
         print("Finished for k = {}".format(k))
         report.plot_loss()
@@ -37,6 +35,6 @@ layer2 = OutputLayer(weights=wi.xavier_initializer(1, 15),
 nn.add_layer(layer1)
 nn.add_layer(layer2)
 
-training_examples = list(zip(train_data[:8], train_label[:8]))
+training_examples = list(zip(train_data[:], train_label[:]))
 #print(training_examples)
-cross_validation(nn, training_examples, 5)
+cross_validation(nn, training_examples, 4)
