55d04891d30be04bdab65da977c0b24d03e87c31
William Simoni <wilsimoni@gmail.com>
2020-12-01 12:01:55 +0100

    Refactored learning_rate,added lr class in NN/monk
    In learning rate I created three classes, one for each of the update method.
    
    I also add the learning rate class and its methods in the Neural Network and Layer class.
    Therefore, I also changed the monk examples that now use the learning_rate class.

--- monk_2_example.py
+++ monk_2_example.py
@@ -7,34 +7,42 @@ from neural_network import NeuralNetwork
 from layer import OutputLayer, HiddenLayer
 import weight_initializer as wi
 import activation_function as af
+import learning_rate as lr
+
 
 def monk_example():
     """
         Test NN model using monk3 dataset
     """
-    nn = NeuralNetwork(400, 'euclidean_loss', 'classification_accuracy', 0.8, nn_type="batch", batch_size=1)
+    nn = NeuralNetwork(400, 'euclidean_loss', 'classification_accuracy',
+                       0.8, nn_type="batch", batch_size=1)
 
-    #create three layers
+    #load data
 
     train_data, train_label, _, _ = read_monk_data("dataset/monks-2.train")
     test_data, test_label, _, _ = read_monk_data("dataset/monks-2.test")
 
+    #create two layers
+
     layer1 = HiddenLayer(weights=wi.he_initializer(15, len(train_data[0])),
-                         learning_rates=np.full((15, len(train_data[0]) + 1),  0.7),
+                         learning_rates=lr.Constant(15, len(train_data[0]), 0.7),
                          activation=af.Relu())
     layer2 = OutputLayer(weights=wi.he_initializer(1, 15),
-                         learning_rates=np.full((1, 16), 0.7),
+                         learning_rates=lr.Constant(1, 15, 0.7),
                          activation=af.Sigmoid())
 
     nn.add_layer(layer1)
     nn.add_layer(layer2)
 
+    #training 
+    
     training_examples = list(zip(train_data, train_label))
     test_examples = list(zip(test_data, test_label))
 
     report = nn.fit(training_examples, test_examples)
     report.plot_loss()
     report.plot_accuracy()
+
 
 if __name__ == "__main__":
     monk_example()
--- monk_3_example.py
+++ monk_3_example.py
@@ -10,6 +10,7 @@ from neural_network import NeuralNetwork
 from layer import OutputLayer, HiddenLayer
 import weight_initializer as wi
 import activation_function as af
+import learning_rate as lr
 
 def monk_example():
     """
@@ -17,21 +18,25 @@ def monk_example():
     """
     nn = NeuralNetwork(200, 'euclidean_loss', 'classification_accuracy', 0.8, 0.005, nn_type="batch", batch_size=1)
 
-    #create three layers
+    #load data
 
-    train_data, train_label, valid_data, valid_label = read_monk_data("dataset/monks-3.train")
+    train_data, train_label, _, _ = read_monk_data("dataset/monks-3.train")
     test_data, test_label, _, _ = read_monk_data("dataset/monks-3.test")
 
+    #create two layers
+
     layer1 = HiddenLayer(weights=wi.xavier_initializer(15, len(train_data[0])),
-                         learning_rates=np.full((15, len(train_data[0]) + 1),  0.8),
+                         learning_rates=lr.Constant(15, len(train_data[0]), 0.8),
                          activation=af.TanH())
     layer2 = OutputLayer(weights=wi.xavier_initializer(1, 15),
-                         learning_rates=np.full((1, 16), 0.8),
+                         learning_rates=lr.Constant(1, 15, 0.8),
                          activation=af.Sigmoid())
 
     nn.add_layer(layer1)
     nn.add_layer(layer2)
 
+    #training 
+    
     training_examples = list(zip(train_data, train_label))
     test_examples = list(zip(test_data, test_label))
 
