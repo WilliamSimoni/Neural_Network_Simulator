c8d36ad76e43b769abe6781cd4533d45f14b747e
William Simoni <wilsimoni@gmail.com>
2020-12-07 19:45:29 +0100

    implemented cartesian product in grid search

--- grid_search.py
+++ grid_search.py
@@ -3,6 +3,8 @@
 """
 from cross_validation import cross_validation
 import multiprocessing
+import itertools
+
 import weight_initializer as wi
 import activation_function as af
 
@@ -15,9 +17,9 @@ parameters = {
     'type_nn': ['batch'],
     'batch_size': [1],
     'topology': [(10, 10), (15, ), (20, 20)],
-    'loss': 'mean_squared_error',
-    'accuracy': 'euclidean_loss',
-    'num_epoch': 500,
+    'loss': ['mean_squared_error'],
+    'accuracy': ['euclidean_loss'],
+    'num_epoch': [500],
 }
 
 
@@ -39,10 +41,10 @@ class GridSearch():
         self.batch_size = parameters['batch_size']
         self.topology = parameters['topology']
         self.num_epoch = parameters['num_epoch']
-        self.loss = parameters['loss_function']
+        self.loss = parameters['loss']
         self.accuracy = parameters['accuracy']
         self.pool = multiprocessing.Pool(multiprocessing.cpu_count()) if n_threads is None else \
-                    multiprocessing.Pool(n_threads)
+            multiprocessing.Pool(n_threads)
         self.results = multiprocessing.Manager().list()
 
     def grid_search(self, save_path='grid_results/grid'):
@@ -53,9 +55,21 @@ class GridSearch():
                 save_path(str): string of file path 
 
         """
-        models_param = self.initialize_models_param()
+        params = [
+            self.learning_rates,
+            self.regularization,
+            self.momentum,
+            self.weight_initialization,
+            self.activation_hidden,
+            self.type_nn,
+            self.batch_size,
+            self.topology,
+            self.num_epoch,
+            self.loss,
+            self.accuracy,
+        ]
 
-        for model_param in models_param:
+        for model_param in itertools.product(*params):
             model = self.initialize_model(model_param)
             self.pool.apply_async(func=run,
                                   args=(model, self.results, model_param))
@@ -63,19 +77,11 @@ class GridSearch():
         self.pool.close()
         self.pool.join()
 
-        #Sort results according to the accuracy of models
+        # Sort results according to the accuracy of models
 
-        #Write to file results obtained
-        
+        # Write to file results obtained
+
         return best_model
-
-    def initialize_models_param(self):
-        """
-            Initialize list of models parameters
-            
-            Return a shuffle list of models parameters to use to initialize a NN object
-            with also layer information
-        """
 
     def initialize_model(self, model_param):
         """
@@ -83,14 +89,17 @@ class GridSearch():
 
             Param: 
                 model_param(dict): dictionary of param to use to create NN object
-            
+
             Return a NN model with also complete graph topology of the network
         """
+        print(model_param)
 
-        
-    def run(self):       
+    def run(self):
         result = cross_validation()
         self.results.append(result, model_param)
 
-    def write_results(self):        
+    def write_results(self):
+        pass
 
+gs = GridSearch(parameters)
+gs.grid_search()
